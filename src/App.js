1d24d0b2d425f603eb625818952041ffc05a6a35
Antony Quintero <antonyquinterog@gmail.com>
2021-08-02 23:11:58 -0500

    Realizando primera version del search box

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ class App extends Component {
 
     this.state = {
       monsters: [],
+      searchField: "",
     };
   }
 
@@ -21,9 +22,19 @@ class App extends Component {
   }
 
   render() {
+    const { monsters, searchField } = this.state;
+    const filterMonsters = monsters.filter((monster) =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+    );
     return (
       <div className="App">
-        <CardList monsters={this.state.monsters} />
+        <input
+          type="search"
+          placeholder="search monsters"
+          onChange={(e) => this.setState({ searchField: e.target.value })}
+        />
+        <tr />
+        <CardList monsters={filterMonsters} />
       </div>
     );
   }
